4acc74d045d23e48f5cdaf1ea32f4b915cb98139
Kathy Bailey Hines <klbailey.k@gmail.com>
2024-02-15 11:00:12 -0700

    Added notes

--- hello.py/helloWorld.py
+++ hello.py/helloWorld.py
@@ -1,9 +1,17 @@
 from flask import Flask
 
+# Create a Flask object (WSGI application)
 app = Flask(__name__)
 
+# Assign URL route
 @app.route("/")
+
+# Create a view function
 def hello_world():
     return "<p>Hello, World!</p>"
+
+# Run the application in main
 if __name__  == '__main__':
    app.run(debug = True, port = 5000)
+
+
