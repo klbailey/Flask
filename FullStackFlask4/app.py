ebd127d2b5adfe6fb7418b7c87be237a3feb44b5
klbai <klbailey.k@gmail.com>
2024-02-26 16:09:28 -0700

    Add delete option to user

--- FullStackFlask4/app.py
+++ FullStackFlask4/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, abort
 from flask_sqlalchemy import SQLAlchemy
 import os #to keep site.db in this project folder
 
